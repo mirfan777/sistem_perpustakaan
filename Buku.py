6138ec45714fe59dc153cf126555de049a506444
Ababil <ababilpuusano@gmail.com>
2023-11-27 16:06:15 +0700

    search by json

--- Buku.py
+++ Buku.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import json
 
 def affected_file(file):
     file_size_before = os.path.getsize(file)
@@ -35,53 +36,55 @@ def hapusBuku():
 def editBuku():
     return
 
-def buatLisBuku():
-    print("\nCari buku berdasarkan:\n1. Judul\n2. Author\n3. Nomor Buku\n")
-    jenis = input("Pilihanmu : ")
+# def buatLisBuku():
+#     print("\nCari buku berdasarkan:\n1. Judul\n2. Author\n3. Nomor Buku\n")
+#     jenis = input("Pilihanmu : ")
 
-    with open("buku.csv", "r") as cariBuku:
-        file = csv.reader(cariBuku)
-        lis = []
+    # with open("buku.csv", "r") as cariBuku:
+    #     file = csv.reader(cariBuku)
+    #     lis = []
+    #     buku = 1
+    #     idbuku = 0
 
-        for row in file:
-            lis.append([row[0], row[1]])
+    #     for row in file:
+    #         lis.append(f"{row[buku].lower()};{row[idbuku]}")
 
-        return(lis)
+    #     return lis
 
 def cariBuku():
-    lis = buatLisBuku()
-    buku = input("\nCari : ")
-    lisbuku = []
-    for i in lis:
-        lisbuku.append(f"{i[1]} {i[0]}")
-    lisbuku.sort()
+    with open("buku.json", "r") as cariBuku:
+        file = json.load(cariBuku)["data"]
+        lis = []
+        namaBuku = 0
+        idBuku = 1
+        buku = input("\nCari : ").lower()
+        for i in file:
+            lis.append(f"{i['judul']};{i['id']}")
+        lis.sort()
 
-    pertama = 0
-    terakhir = len(lis)-1
-    # ketemu = False
-    ada = False
-    lisear = []
+        pertama = 0
+        terakhir = len(file) - 1
+        ada = False
+        lisear = []
 
-    while pertama<=terakhir and not ada:
-        tengah = (pertama+terakhir)//2
-        center = lisbuku[tengah].split(" ")[0]
-        if center == buku:
-            ada = True
-            counter = 1
-            for i in lisbuku:
-                if buku in i:
-                    lisear.append(f"{counter+1}. {i.split()[1]}\nID Buku : {i.split()[0]}\n")
-                    counter+=1
-        else:
-            if buku < lisbuku[tengah]:
-                terakhir -=1
+        while pertama <= terakhir and not ada:
+            tengah = (pertama + terakhir) // 2
+            center = lis[tengah].lower()
+            if buku in center:
+                ada = True
+                counter = 1
+                for i in lis:
+                    if buku in i.lower():
+                        lisear.append(f"{counter}. {i.split(';')[namaBuku]}\nID Buku : {i.split(';')[idBuku]} \n")
+                        counter += 1
             else:
-                pertama +=1
+                if buku < center:
+                    terakhir = tengah - 1
+                else:
+                    pertama = tengah + 1
 
-    print("\nHasil :\n")
-    for i in lisear:
-        print(i)
-
-    pilihan = input("Pilihanmu : ")
+        print("\nHasil :\n")
+        for i in lisear:
+            print(i)
 
 cariBuku()
