45018fa0a75efc16bc4ca40acb52a1ffa0e68168
Ababil <ababilpuusano@gmail.com>
2023-11-30 12:26:28 +0700

    faris

--- keanggotaan3.py
+++ keanggotaan3.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 class AnggotaPerpustakaan:
     def __init__(self, id_anggota, nama, email, telepon, username):
@@ -9,25 +10,25 @@ class AnggotaPerpustakaan:
         self.username = username
 
 class Perpustakaan:
-    def __init__(self, json_file="anggota.json"):
-        self.json_file = json_file
-        self.daftar_anggota = []
-        self._load_from_json()
+    def __init__(self, file="anggota.json"):
+        self.file = file
+        self.daftarAnggota = []
+        self.loadJson()
 
-    def _load_from_json(self):
+    def loadJson(self):
         try:
-            with open(self.json_file, 'r') as file:
+            with open(self.file, 'r') as file:
                 data = json.load(file)
                 for item in data:
                     anggota = AnggotaPerpustakaan(item['id_anggota'], item['nama'], item['email'], item['telepon'], item['username'])
-                    self.daftar_anggota.append(anggota)
+                    self.daftarAnggota.append(anggota)
         except FileNotFoundError:
             pass
 
-    def _save_to_json(self):
-        with open(self.json_file, 'w') as file:
+    def simpan(self):
+        with open(self.file, 'w') as file:
             data = []
-            for anggota in self.daftar_anggota:
+            for anggota in self.daftarAnggota:
                 data.append({
                     'id_anggota': anggota.id_anggota,
                     'nama': anggota.nama,
@@ -38,34 +39,34 @@ class Perpustakaan:
             json.dump(data, file, indent=2)
 
     def tambah_anggota(self, anggota):
-        self.daftar_anggota.append(anggota)
-        self._save_to_json()
-        print("Anggota ditambahkan!")
+        self.daftarAnggota.append(anggota)
+        self.simpan()
+        print("\nAnggota ditambahkan!")
 
     def hapus_anggota(self, id_anggota):
-        for anggota in self.daftar_anggota:
+        for anggota in self.daftarAnggota:
             if anggota.id_anggota == id_anggota:
-                self.daftar_anggota.remove(anggota)
-                self._save_to_json()
-                print("Anggota dihapus!")
+                self.daftarAnggota.remove(anggota)
+                self.simpan()
+                print("\nAnggota dihapus!")
                 return
-        print("ID Anggota tidak ditemukan.")
+        print("\nID Anggota tidak ditemukan.")
 
     def edit_anggota(self, id_anggota, nama, email, telepon, username):
-        for anggota in self.daftar_anggota:
+        for anggota in self.daftarAnggota:
             if anggota.id_anggota == id_anggota:
                 anggota.nama = nama
                 anggota.email = email
                 anggota.telepon = telepon
                 anggota.username = username
-                self._save_to_json()
-                print("Data anggota diubah!")
+                self.simpan()
+                print("\nData anggota diubah!")
                 return
-        print("ID Anggota tidak ditemukan.")
+        print("\nID Anggota tidak ditemukan.")
 
     def tampilkan_anggota(self):
         print("\nDaftar Anggota Perpustakaan:")
-        for anggota in self.daftar_anggota:
+        for anggota in self.daftarAnggota:
             print(f"ID: {anggota.id_anggota}, Nama: {anggota.nama}, email: {anggota.email}, Telepon: {anggota.telepon}, Username: {anggota.username}")
             
 
@@ -83,29 +84,35 @@ while True:
     pilihan = input("Masukkan pilihan (0-4): ")
 
     if pilihan == "1":
-        id_anggota = (input("Masukkan ID Anggota: "))
+        id_anggota = str(time.time())
         nama = input("Masukkan Nama: ")
-        email = input("Masukkan Email: ")
+        while True:
+            email = input("Masukkan Email: ")
+            if not email.endswith('@gmail.com'):
+                print("Email harus diakhiri dengan '@gmail.com'. Perubahan dibatalkan.")
+            else:
+                break
         telepon = input("Masukkan Nomor Telepon: ")
         username = input("Masukkan username pengguna :")
         anggota_baru = AnggotaPerpustakaan(id_anggota, nama, email, telepon, username)
         perpustakaan.tambah_anggota(anggota_baru)
 
     elif pilihan == "2":
-        id_anggota = int(input("Masukkan ID Anggota yang akan dihapus: "))
+        id_anggota = int(input("Masukkan ID Anggota yang akan dihapus (Berupa angka): "))
         perpustakaan.hapus_anggota(id_anggota)
 
     elif pilihan == "3":
         id_anggota = (input("Masukkan ID Anggota yang akan diubah: "))
         nama = input("Masukkan Nama Baru: ")
-        email = input("Masukkan email Baru: ")
-        telepon = int(input("Masukkan Nomor Telepon Baru: "))
+        while True:
+            email = input("Masukkan email Baru: ")
+            if not email.endswith('@gmail.com'):
+                print("Email harus diakhiri dengan '@gmail.com'. Perubahan dibatalkan.")
+            else:
+                break
+        telepon = input("Masukkan Nomor Telepon Baru (Angka): ")
         username = input("Masukkan Username Baru: ")
         perpustakaan.edit_anggota(id_anggota, nama, email, telepon, username)
-        
-        if not email.endswith('@gmail.com'):
-            print("Email harus diakhiri dengan '@gmail.com'. Perubahan dibatalkan.")
-            continue
 
     elif pilihan == "4":
         perpustakaan.tampilkan_anggota()
