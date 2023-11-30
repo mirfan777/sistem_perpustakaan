bc05afecb60602954d560a9db0430dd934903eca
Ababil <ababilpuusano@gmail.com>
2023-11-30 09:14:26 +0700

    Revisi2

--- Buku.py
+++ Buku.py
@@ -36,50 +36,8 @@ def hapusBuku():
 def editBuku():
     return
 
-# def cariBuku():
-#     with open("buku.json", "r") as cariBuku:
-#         file = json.load(cariBuku)["data"]
-#         lis = []
-#         namaBuku = 0
-#         idBuku = 1
-#         authorBuku = 2
-#         genreBuku = 3
-
-#         print("Cari buku berdasarkan :\n1. Judul Buku\n2. ID Buku\n3. Author/Penulis\n")
-#         filter = input("Pilihanmu : ")
-
-#         buku = input("\nCari : ").lower()
-#         for i in file:
-#             lis.append(f"{i['judul']};{i['id']};{i['author']};{i['genre']}")
-#         lis.sort()
-
-#         pertama = 0
-#         terakhir = len(file) - 1
-#         ada = False
-#         lisear = []
-
-#         while pertama <= terakhir and not ada:
-#             tengah = (pertama + terakhir) // 2
-#             center = lis[tengah].split(";")[0].lower()
-#             if buku in center:
-#                 ada = True
-#                 counter = 1
-#                 for i in lis:
-#                     if buku in i.lower():
-#                         lisear.append(f"{counter}. {i.split(';')[namaBuku]}\nID Buku : {i.split(';')[idBuku]} \n")
-#                         counter += 1
-#             else:
-#                 if buku < center:
-#                     terakhir = tengah - 1
-#                 else:
-#                     pertama = tengah + 1
-
-#         print("\nHasil :\n")
-#         for i in lisear:
-#             print(i)
-
 def cariBuku():
-    with open("buku.j son", "r") as cariBuku:
+    with open("buku.json", "r") as cariBuku:
         file = json.load(cariBuku)["data"]
         counter = 1
         lisear = []
@@ -102,14 +60,64 @@ def cariBuku():
                 lisear.append(f"{counter}. {i['judul']}\nID Buku : {i['id']}\nAuthor : {i['author']} genre : {'Tidak ada genre' if len(i['genre'])==0 else ','.join(i['genre'])}\n")
                 counter +=1
 
-        # if len(lisear)
-
         print("\nHasil :\n")
         if len(lisear)==0:
             print("Tidak ditemukan.")
         else:
-            for i in lisear:
-                print(i)
+            if len(lisear)<=10:
+                for i in lisear:
+                    print(i)
+            else:
+                awal = 0
+                akhir = 10
+                while True:
+                    for i in lisear[awal:akhir]:
+                        print(i)
+                    print("\nN untuk Next, B untuk Back, E untuk exit\n")
+                    pagi = input("Pilihanmu : ").lower()
+                    salah = False
+                    if pagi == "n":
+                        if len(lisear)-akhir <= 0:
+                            print("Sudah Page Akhir\n")
+                            salah = True
+                        else:
+                            awal += 10
+                            akhir += 10
+                    elif pagi == "b" :
+                        if awal == 0:
+                            print("Sudah Page Awal\n")
+                            salah = True
+                        else:
+                            awal -= 10
+                            akhir -= 10
+                    elif pagi == "e":
+                        break
+                    else:
+                        print("Pilihan tidak valid.\n")
+                    print(awal, akhir)
+                    while salah :
+                        pagi = input("Pilihanmu : ")
+                        if pagi == "n":
+                            if len(lisear)-akhir <= 0:
+                                print("Sudah Page Akhir\n")
+                            else:
+                                awal += 10
+                                akhir += 10
+                                salah = False
+                        elif pagi == "b" :
+                            if awal == 0:
+                                print("Sudah Page Awal\n")
+                            else:
+                                awal -= 10
+                                akhir -= 10
+                                salah = False
+                        elif pagi == "e":
+                            break
+                        else:
+                            print("Pilihan tidak valid.\n")
+                        print(awal, akhir)
+                    if pagi == "e":
+                        break
 
 def sortBuku():
     with open("buku.json", "r") as sortBuku:
